99ded0adc9f460bfcb2eab2894f9eb6a13e94599
redwallhp <mcredwallhp@gmail.com>
2016-09-17 04:29:22 -0400

    Fixed Scunthorpe Problem in Responses module by allowing the phrases to contain regex.

--- plugins/responses.py
+++ plugins/responses.py
@@ -2,21 +2,25 @@ import discord
 from discord.ext import commands
 from lib import configurator
 import asyncio
+import re
 
 class Responses:
 
     def __init__(self, bot):
         self.bot = bot
         self.phrases = configurator.getDict('responses', {})
+        self.regexes = {}
+        for phrase in self.phrases:
+            self.regexes[phrase] = (re.compile(phrase, re.IGNORECASE))
         self.bot.async_event('on_message')
 
     async def on_message(self, msg):
         if msg.author.bot:
             return
         for phrase, response in self.phrases.items():
-            if phrase.lower() in msg.content.lower():
+            regex = self.regexes[phrase]
+            if regex.search(msg.content) != None:
                 await self.bot.send_message(msg.channel, response)
-
-
+    
 def setup(bot):
     bot.add_cog(Responses(bot))
